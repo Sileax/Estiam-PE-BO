a19cd7ee232b2992454f5b793ac9996dc760f29a
Unknown <Aerys59@gmail.com>
2017-04-10 11:11:18 +0200

    update shipping

--- views/ListShippingMethods.jsx
+++ views/ListShippingMethods.jsx
@@ -3,7 +3,7 @@ var connect = require('react-redux').connect;
 require('fetch-everywhere');
 var Modal = require('react-modal');
 var Notification = require('react-notification').Notification;
-const apiUrl = "http://193.70.40.193:3000/api";
+const apiUrl = "http://localhost:3000/api";
 
 class ListShippingMethods extends React.Component {
 
@@ -70,6 +70,7 @@ class ListShippingMethods extends React.Component {
         }).then((response) => {
             return response.json()
         }).then((json) => {
+            console.log(json);
             self.closeModal();
             self.toggleNotification();
             self.getShipping();
